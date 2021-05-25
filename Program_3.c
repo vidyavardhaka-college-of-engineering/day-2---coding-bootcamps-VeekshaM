cf18d35fe3bd4a720089bf57090090c9fb6aecf4
VeekshaM <>
2021-05-25 11:42:03 +0000

    char symbol='#'

--- Program_3.c
+++ Program_3.c
@@ -3,10 +3,10 @@
 int main()
 {
 //Write your code bellow
-
+char symbol = '#';
 
 //if you write correct code fallowing output will print
-printf("%c\n",c);
+printf("%c\n",symbol);
 printf("Good job..");
 return 0;
 }
